1a24603d380decabb915def8f2e6b6e423efef20
mtleinon <mika.t.leinonen@gmail.com>
2018-11-29 21:35:47 +0200

    Updates

---
+++ precache-manifest.9aea948f5c03c70dbd7a01560c26d250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "2006cb432d4e7edff45e",
+    "url": "/robofriends/static/js/main.2006cb43.chunk.js"
+  },
+  {
+    "revision": "7d062a0d54e447194fc7",
+    "url": "/robofriends/static/js/1.7d062a0d.chunk.js"
+  },
+  {
+    "revision": "2006cb432d4e7edff45e",
+    "url": "/robofriends/static/css/main.9325c17d.chunk.css"
+  },
+  {
+    "revision": "7d062a0d54e447194fc7",
+    "url": "/robofriends/static/css/1.25f0cd93.chunk.css"
+  },
+  {
+    "revision": "0de237c68c29f390eeb507af88dfeb7f",
+    "url": "/robofriends/index.html"
+  }
+];
